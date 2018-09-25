b0f7d2387363a4f776e32bfd9d6d428f05677f6e
Asim Hirji <asimhirji@gmail.com>
2018-09-25 16:20:22 -0500

    collection view changes

---
+++ PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Asim Hirji on 9/25/18.
+//  Copyright © 2018 Asim Hirji. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+}
